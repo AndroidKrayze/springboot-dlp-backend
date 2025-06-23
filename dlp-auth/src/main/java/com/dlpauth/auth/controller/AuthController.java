9ca81821f2da296ad7a9b21d69937c9781ae6d0d
AndroidKrayze <androidkrayze@gmail.com>
2025-06-23 21:37:05 +0530

    Update AuthController.java

--- dlp-auth/src/main/java/com/dlpauth/auth/controller/AuthController.java
+++ dlp-auth/src/main/java/com/dlpauth/auth/controller/AuthController.java
@@ -1,23 +1,30 @@
 package com.dlpauth.auth.controller;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dlpauth.exception.ErrorDetails;
 import com.dlpauth.model.DlpauthOutputResult;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+	 @PostConstruct
+  	public void init() {
+        	log.info(" AuthController loaded");
+  	  }
+
 	@GetMapping("/userInfo")
 	public DlpauthOutputResult getCurrentUser(@AuthenticationPrincipal OidcUser oidcUser) {
+		log.info(" /auth/userInfo endpoint called");
 	    DlpauthOutputResult output = new DlpauthOutputResult();
 
 	    if (oidcUser == null) {
