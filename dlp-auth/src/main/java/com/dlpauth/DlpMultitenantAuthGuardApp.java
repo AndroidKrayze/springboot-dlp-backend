ee29eb60bd0a08cf8a3b09ffe358995a1b0ce643
AndroidKrayze <androidkrayze@gmail.com>
2025-06-23 21:27:09 +0530

    Update DlpMultitenantAuthGuardApp.java

--- dlp-auth/src/main/java/com/dlpauth/DlpMultitenantAuthGuardApp.java
+++ dlp-auth/src/main/java/com/dlpauth/DlpMultitenantAuthGuardApp.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.*")
+@ComponentScan(basePackages = "com.dlpauth")
 @SpringBootApplication
 public class DlpMultitenantAuthGuardApp {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DlpMultitenantAuthGuardApp.class, args);
-		
-		
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DlpMultitenantAuthGuardApp.class, args);
+    }
 }
+
 
 
